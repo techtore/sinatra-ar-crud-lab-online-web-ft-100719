e8f0755578d66c1fc1a9e6560e9ec94bb59d1ec5
Victoria Hale <vic.hale74@gmail.com>
2019-11-21 17:08:48 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -34,10 +34,10 @@ class ApplicationController < Sinatra::Base
     erb :edit
   end 
   
-  patch '/articles/:id' do
+   patch "/articles/:id" do
     @article = Article.find(params[:id])
     @article.update(params[:article])
-    redirect to "/articles/#{@article.id}"
+    redirect to "/articles/#{ @article.id }"
   end
 
   
